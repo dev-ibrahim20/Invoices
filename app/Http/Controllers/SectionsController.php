52ad1078687ff4a53d4d82ed6cd48e18597cc05f
Ibrahem Al-ashqar <dev.ibrahem@dev.ibrahem.mohamed@gmail.com>
2025-11-02 07:49:58 +0200

    Edit And Delete Sections Data

--- app/Http/Controllers/SectionsController.php
+++ app/Http/Controllers/SectionsController.php
@@ -36,20 +36,20 @@ class SectionsController extends Controller
      */
     public function store(Request $request)
     {
-        $input = $request->all();
-        $b_exists = sections::where('section_name', $input['section_name'])->exists();
-        if ($b_exists) {
-            session()->flash('error', 'Section already exists');
-            return redirect('/sections');
-        } else {
-            sections::create([
-                'section_name' => $input['section_name'],
-                'description' => $input['description'],
-                'created_by' => auth()->user()->name,
-            ]);
-            session()->flash('Add', 'Section added successfully');
-            return redirect('/sections');
-        }
+        $validatedData = $request->validate([
+            'section_name' => 'required|unique:sections|max:255',
+            'description' => 'required',
+        ], [
+            'section_name.required' => 'Please enter the section name.',
+            'section_name.unique' => 'This section name already exists.',
+            'description.required' => 'Please enter the description.',
+        ]);
+        sections::create([
+            'section_name' => $request['section_name'],
+            'description' => $request['description'],
+            'created_by' => auth()->user()->name,
+        ]);
+        return redirect('/sections');
     }
 
     /**
@@ -83,7 +83,28 @@ class SectionsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $id = $request->id;
+
+        $this->validate($request, [
+
+            'section_name' => 'required|max:255|unique:sections,section_name,' . $id,
+            'description' => 'required',
+        ], [
+
+            'section_name.required' => 'يرجي ادخال اسم القسم',
+            'section_name.unique' => 'اسم القسم مسجل مسبقا',
+            'description.required' => 'يرجي ادخال البيان',
+
+        ]);
+
+        $sections = sections::find($id);
+        $sections->update([
+            'section_name' => $request->section_name,
+            'description' => $request->description,
+        ]);
+
+        session()->flash('edit', 'تم تعديل القسم بنجاج');
+        return redirect('/sections');
     }
 
     /**
@@ -92,8 +113,11 @@ class SectionsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request)
     {
-        //
+        $id = $request->id;
+        sections::find($id)->delete();
+        session()->flash('delete', 'تم حذف القسم بنجاح');
+        return redirect('/sections');
     }
 }
--- resources/views/sections/sections.blade.php
+++ resources/views/sections/sections.blade.php
@@ -29,26 +29,46 @@ Sections - Invoices Program
 				<!-- breadcrumb -->
 @endsection
 @section('content')
+
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            @if (session()->has('Add'))
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <strong>{{ session()->get('Add') }}</strong>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            @endif
+
+            @if (session()->has('edit'))
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <strong>{{ session()->get('edit') }}</strong>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            @endif
+
+            @if (session()->has('delete'))
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <strong>{{ session()->get('delete') }}</strong>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            @endif
+
             <!-- row -->
             <div class="row">
-
-                @if (session()->has('Add'))
-                    <div class="alert alert-success alert-dismissible fade show" role="alert">
-                        <strong>{{ session()->get('Add') }}</strong>
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                @endif
-                @if (session()->has('error'))
-                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        <strong>{{ session()->get('error') }}</strong>
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                @endif
-
                 <div class="col-xl-12">
                     <div class="card mg-b-20">
                                 <a class="modal-effect btn btn-outline-primary btn-block" data-effect="effect-scale" data-toggle="modal"
@@ -75,7 +95,18 @@ Sections - Invoices Program
                                                 <td>{{ $section->section_name }}</td>
                                                 <td>{{$section->description}}</td>
                                                 <td>{{ $section->created_by }}</td>
-                                                <td>61</td>
+                                                <td>
+                                                    <a class="modal-effect btn btn-sm btn-info" data-effect="effect-scale"
+                                                        data-id="{{ $section->id }}" data-section_name="{{ $section->section_name }}"
+                                                        data-description="{{ $section->description }}" data-toggle="modal"
+                                                        href="#exampleModal2" title="تعديل"><i class="las la-pen"></i></a>
+
+                                                    <a class="modal-effect btn btn-sm btn-danger" data-effect="effect-scale"
+                                                        data-id="{{ $section->id }}" data-section_name="{{ $section->section_name }}"
+                                                        data-toggle="modal" href="#modaldemo9" title="حذف"><i
+                                                            class="las la-trash"></i></a>
+
+                                                </td>
                                             </tr>
                                         @endforeach
                                     </tbody>
@@ -122,34 +153,119 @@ Sections - Invoices Program
                 <!-- End Modal effects-->
             <!-- row closed -->
         </div>
+        <!-- edit -->
+        <div class="modal fade" id="exampleModal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">تعديل القسم</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+
+                        <form action="sections/update" method="post" autocomplete="off">
+                            {{ method_field('patch') }}
+                            {{ csrf_field() }}
+                            <div class="form-group">
+                                <input type="hidden" name="id" id="id" value="">
+                                <label for="recipient-name" class="col-form-label">اسم القسم:</label>
+                                <input class="form-control" name="section_name" id="section_name" type="text">
+                            </div>
+                            <div class="form-group">
+                                <label for="message-text" class="col-form-label">ملاحظات:</label>
+                                <textarea class="form-control" id="description" name="description"></textarea>
+                            </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">تاكيد</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">اغلاق</button>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+            <!-- delete -->
+            <div class="modal" id="modaldemo9">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content modal-content-demo">
+                        <div class="modal-header">
+                            <h6 class="modal-title">حذف القسم</h6><button aria-label="Close" class="close" data-dismiss="modal"
+                                type="button"><span aria-hidden="true">&times;</span></button>
+                        </div>
+                        <form action="sections/destroy" method="post">
+                            {{ method_field('delete') }}
+                            {{ csrf_field() }}
+                            <div class="modal-body">
+                                <p>هل انت متاكد من عملية الحذف ؟</p><br>
+                                <input type="hidden" name="id" id="id" value="">
+                                <input class="form-control" name="section_name" id="section_name" type="text" readonly>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">الغاء</button>
+                                <button type="submit" class="btn btn-danger">تاكيد</button>
+                            </div>
+                    </div>
+                    </form>
+                </div>
+            </div>
         <!-- Container closed -->
     </div>
     <!-- main-content closed -->
 @endsection
 @section('js')
-    <!-- Internal Data tables -->
-    <script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
-    <script src="{{URL::asset('assets/plugins/datatable/js/dataTables.dataTables.min.js')}}"></script>
-    <script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
-    <script src="{{URL::asset('assets/plugins/datatable/js/responsive.dataTables.min.js')}}"></script>
-    <script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.js')}}"></script>
-    <script src="{{URL::asset('assets/plugins/datatable/js/dataTables.bootstrap4.js')}}"></script>
-    <script src="{{URL::asset('assets/plugins/datatable/js/dataTables.buttons.min.js')}}"></script>
-    <script src="{{URL::asset('assets/plugins/datatable/js/buttons.bootstrap4.min.js')}}"></script>
-    <script src="{{URL::asset('assets/plugins/datatable/js/jszip.min.js')}}"></script>
-    <script src="{{URL::asset('assets/plugins/datatable/js/pdfmake.min.js')}}"></script>
-    <script src="{{URL::asset('assets/plugins/datatable/js/vfs_fonts.js')}}"></script>
-    <script src="{{URL::asset('assets/plugins/datatable/js/buttons.html5.min.js')}}"></script>
-    <script src="{{URL::asset('assets/plugins/datatable/js/buttons.print.min.js')}}"></script>
-    <script src="{{URL::asset('assets/plugins/datatable/js/buttons.colVis.min.js')}}"></script>
-    <script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
-    <script src="{{URL::asset('assets/plugins/datatable/js/responsive.bootstrap4.min.js')}}"></script>
-    <!--Internal  Datatable js -->
-    <script src="{{URL::asset('assets/js/table-data.js')}}"></script>
-    <!--Internal  Datepicker js -->
-    <script src="{{URL::asset('assets/plugins/jquery-ui/ui/widgets/datepicker.js')}}"></script>
-    <!-- Internal Select2 js-->
-    <script src="{{URL::asset('assets/plugins/select2/js/select2.min.js')}}"></script>
-    <!-- Internal Modal js-->
-    <script src="{{URL::asset('assets/js/modal.js')}}"></script>
+        <!-- Internal Data tables -->
+        <script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
+        <script src="{{URL::asset('assets/plugins/datatable/js/dataTables.dataTables.min.js')}}"></script>
+        <script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+        <script src="{{URL::asset('assets/plugins/datatable/js/responsive.dataTables.min.js')}}"></script>
+        <script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.js')}}"></script>
+        <script src="{{URL::asset('assets/plugins/datatable/js/dataTables.bootstrap4.js')}}"></script>
+        <script src="{{URL::asset('assets/plugins/datatable/js/dataTables.buttons.min.js')}}"></script>
+        <script src="{{URL::asset('assets/plugins/datatable/js/buttons.bootstrap4.min.js')}}"></script>
+        <script src="{{URL::asset('assets/plugins/datatable/js/jszip.min.js')}}"></script>
+        <script src="{{URL::asset('assets/plugins/datatable/js/pdfmake.min.js')}}"></script>
+        <script src="{{URL::asset('assets/plugins/datatable/js/vfs_fonts.js')}}"></script>
+        <script src="{{URL::asset('assets/plugins/datatable/js/buttons.html5.min.js')}}"></script>
+        <script src="{{URL::asset('assets/plugins/datatable/js/buttons.print.min.js')}}"></script>
+        <script src="{{URL::asset('assets/plugins/datatable/js/buttons.colVis.min.js')}}"></script>
+        <script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+        <script src="{{URL::asset('assets/plugins/datatable/js/responsive.bootstrap4.min.js')}}"></script>
+        <!--Internal  Datatable js -->
+        <script src="{{URL::asset('assets/js/table-data.js')}}"></script>
+        <!--Internal  Datepicker js -->
+        <script src="{{URL::asset('assets/plugins/jquery-ui/ui/widgets/datepicker.js')}}"></script>
+        <!-- Internal Select2 js-->
+        <script src="{{URL::asset('assets/plugins/select2/js/select2.min.js')}}"></script>
+        <!-- Internal Modal js-->
+        <script src="{{URL::asset('assets/js/modal.js')}}"></script>
+
+        <script>
+            $('#exampleModal2').on('show.bs.modal', function (event) {
+                var button = $(event.relatedTarget)
+                var id = button.data('id')
+                var section_name = button.data('section_name')
+                var description = button.data('description')
+                var modal = $(this)
+                modal.find('.modal-body #id').val(id);
+                modal.find('.modal-body #section_name').val(section_name);
+                modal.find('.modal-body #description').val(description);
+            })
+
+        </script>
+
+
+    <script>
+        $('#modaldemo9').on('show.bs.modal', function (event) {
+            var button = $(event.relatedTarget)
+            var id = button.data('id')
+            var section_name = button.data('section_name')
+            var modal = $(this)
+            modal.find('.modal-body #id').val(id);
+            modal.find('.modal-body #section_name').val(section_name);
+        })
+
+    </script>
 @endsection
