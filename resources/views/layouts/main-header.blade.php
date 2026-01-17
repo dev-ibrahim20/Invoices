32a1ad320075d5947cedd5f1fc270c16238bb997
Ibrahem Al-ashqar <dev.ibrahem@dev.ibrahem.mohamed@gmail.com>
2026-01-17 12:16:16 +0200

    clean My project

--- resources/views/layouts/main-header.blade.php
+++ resources/views/layouts/main-header.blade.php
@@ -253,11 +253,11 @@
                 </div>
                 <div class="dropdown main-profile-menu nav nav-item nav-link">
                     <a class="profile-user d-flex" href=""><img alt=""
-                            src="{{ URL::asset('assets/img/faces/6.jpg') }}"></a>
+                            src="{{ URL::asset('assets/img/faces/66.jpg') }}"></a>
                     <div class="dropdown-menu">
                         <div class="main-header-profile bg-primary p-3">
                             <div class="d-flex wd-100p">
-                                <div class="main-img-user"><img alt="" src="{{ URL::asset('assets/img/faces/6.jpg') }}"
+                                <div class="main-img-user"><img alt="" src="{{ URL::asset('assets/img/faces/66.jpg') }}"
                                         class=""></div>
                                 <div class="mr-3 my-auto">
                                     <h6>{{ Auth::user()->name }}</h6><span>{{ Auth::user()->email }}</span>
